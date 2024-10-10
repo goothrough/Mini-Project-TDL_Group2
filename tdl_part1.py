c25268e67c1343e0475b9a48ff2c4dc8afdc51e5
Suguru Ohashi <goothrough315@gmail.com>
2024-10-10 14:17:58 -0700

    proto type code

---
+++ tdl_part1.py
@@ -0,0 +1,29 @@
+task_list = []
+
+while True:
+    print("To-Do List Application")
+    print("1. Add Task")
+    print("2. Remove Task")
+    print("3. View Tasks")
+    print("4. Exit")
+    user_input = input("Enter your choice: ")
+    user_input = int(user_input)
+
+    if user_input == 1:
+        # Write the Add Task logic
+        
+        continue
+    
+    if user_input == 2:
+        # Write the Remove Task logic
+        
+        continue
+    
+    if user_input == 3:
+        # Write the View Tasks logic
+        
+        continue
+    
+    if user_input == 4:
+        # Finish the program
+        break
